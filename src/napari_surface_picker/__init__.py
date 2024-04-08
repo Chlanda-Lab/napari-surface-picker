edb6e283c6db0a6319f6fee99fed7057722ded35
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2024-04-08 14:03:35 +0200

    Add widget test

--- src/napari_surface_picker/__init__.py
+++ src/napari_surface_picker/__init__.py
@@ -1,11 +1,8 @@
 __version__ = "0.0.1"
-from ._sample_data import make_sample_data
+from ._sample_data import make_sample_mesh
 from ._widget import SurfacePicker
-from ._writer import write_multiple, write_single_image
 
 __all__ = (
-    "write_single_image",
-    "write_multiple",
-    "make_sample_data",
+    "make_sample_mesh",
     "SurfacePicker",
 )
--- src/napari_surface_picker/_tests/test_sample_data.py
+++
@@ -1,7 +0,0 @@
-# from napari_surface_picker import make_sample_data
-
-# add your tests here...
-
-
-def test_something():
-    pass
