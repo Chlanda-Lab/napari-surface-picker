cd164c152ad6973984a888902c2fbde14589c5d1
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2024-04-09 16:47:40 +0200

    Allow saving multiple particle layers

--- src/napari_surface_picker/_writer.py
+++ src/napari_surface_picker/_writer.py
@@ -1,8 +1,5 @@
-import dynamotable.constants
-import dynamotable.utils
 import numpy as np
 import starfile
-import dynamotable
 import pandas as pd
 from typing import TYPE_CHECKING, Any, List, Sequence, Tuple, Union
 from .utils import vec2euler
@@ -12,16 +9,20 @@ if TYPE_CHECKING:
     FullLayerData = Tuple[DataType, dict, str]
 
 
-def write_starfile(path: str, data: Any, meta: dict) -> List[str]:
-    angles = np.rad2deg(vec2euler(data[:, 1, :], True))
-    particles = pd.DataFrame(data={
-        "rlnCoordinateX": data[:, 0, 2],
-        "rlnCoordinateY": data[:, 0, 1],
-        "rlnCoordinateZ": data[:, 0, 0],
-        "rlnAngleRot": angles[:, 0],
-        "rlnAngleTilt": angles[:, 1],
-        "rlnAnglePsi": angles[:, 2],
-        # "rlnTomoName": meta["name"].replace(" ", "_"),
-    })
+def write_starfile(path: str, data: List["FullLayerData"]) -> List[str]:
+    dfs = []
+    for layer_data, meta, _layer_type in data:
+        angles = np.rad2deg(vec2euler(layer_data[:, 1, :], True))
+        df = pd.DataFrame(data={
+            "rlnCoordinateX": layer_data[:, 0, 2],
+            "rlnCoordinateY": layer_data[:, 0, 1],
+            "rlnCoordinateZ": layer_data[:, 0, 0],
+            "rlnAngleRot": angles[:, 0],
+            "rlnAngleTilt": angles[:, 1],
+            "rlnAnglePsi": angles[:, 2],
+            "rlnMicrographName": meta["name"].replace(" ", "_"),
+        })
+        dfs.append(df)
+    particles = pd.concat(dfs)
     starfile.write(particles, path, overwrite=True)
     return [path]
