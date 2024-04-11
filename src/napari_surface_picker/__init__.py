94deb7c9f3bcd213f32981747bdbaeb410288233
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2024-04-11 18:11:35 +0200

    Add Relion tomo writer

--- src/napari_surface_picker/__init__.py
+++ src/napari_surface_picker/__init__.py
@@ -2,11 +2,12 @@ __version__ = "0.0.1"
 from ._sample_data import make_sample_mesh
 from ._surface_picker import SurfacePicker
 from ._dipole_picker import DipolePicker
-from ._writer import write_starfile
+from ._writer import write_star_reliontomo, write_star_warp
 
 __all__ = (
     "make_sample_mesh",
     "SurfacePicker",
     "DipolePicker",
-    "write_starfile",
+    "write_star_reliontomo",
+    "write_star_warp",
 )
--- src/napari_surface_picker/_writer.py
+++ src/napari_surface_picker/_writer.py
@@ -9,20 +9,45 @@ if TYPE_CHECKING:
     FullLayerData = Tuple[DataType, dict, str]
 
 
-def write_starfile(path: str, data: List["FullLayerData"]) -> List[str]:
+def _layers_to_df(
+    data: List["FullLayerData"], layer_column_name: str
+) -> pd.DataFrame:
+    """Turn a list of FullLayerData of vectors into a DataFrame with particle
+    coordinates.
+    """
     dfs = []
     for layer_data, meta, _layer_type in data:
         angles = np.rad2deg(vec2euler(layer_data[:, 1, :], True))
-        df = pd.DataFrame(data={
-            "rlnCoordinateX": layer_data[:, 0, 2],
-            "rlnCoordinateY": layer_data[:, 0, 1],
-            "rlnCoordinateZ": layer_data[:, 0, 0],
-            "rlnAngleRot": angles[:, 0],
-            "rlnAngleTilt": angles[:, 1],
-            "rlnAnglePsi": angles[:, 2],
-            "rlnMicrographName": meta["name"].replace(" ", "_"),
-        })
+        df = pd.DataFrame(
+            data={
+                "rlnCoordinateX": layer_data[:, 0, 2],
+                "rlnCoordinateY": layer_data[:, 0, 1],
+                "rlnCoordinateZ": layer_data[:, 0, 0],
+                "rlnAngleRot": angles[:, 0],
+                "rlnAngleTilt": angles[:, 1],
+                "rlnAnglePsi": angles[:, 2],
+                layer_column_name: meta["name"].replace(" ", "_"),
+            }
+        )
         dfs.append(df)
-    particles = pd.concat(dfs)
+    return pd.concat(dfs)
+
+
+def _ensure_suffix(string: str, suffix: str) -> str:
+    if string.endswith(suffix):
+        return string
+    return string + suffix
+
+
+def write_star_warp(path: str, data: List["FullLayerData"]) -> List[str]:
+    path = _ensure_suffix(path, ".star")
+    particles = _layers_to_df(data, "rlnMicrographName")
     starfile.write(particles, path, overwrite=True)
+    return [path]
+
+
+def write_star_reliontomo(path: str, data: List["FullLayerData"]) -> List[str]:
+    path = _ensure_suffix(path, ".star")
+    particles = _layers_to_df(data, "rlnTomoName")
+    starfile.write({"particles": particles}, path, overwrite=True)
     return [path]
